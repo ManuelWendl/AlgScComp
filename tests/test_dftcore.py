ae5b7bcaf6a1e6f8e803bb01dee55c9e76b156d1
Manuel Wendl <wendlmanuel@yahoo.de>
2023-10-04 08:49:16 +0200

    Implementation of timing for fft algotithms

--- tests/test_dftcore.py
+++ tests/test_dftcore.py
@@ -2,69 +2,133 @@ import AlgScComp as asc
 import numpy as np
 import time 
 
-a = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]
-b = [8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0]
+testRuntime = True
+
+if testRuntime == False:
+    a = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]
+    b = [8.0,7.0,6.0,5.0,4.0,3.0,2.0,1.0]
+else:
+    a = np.random.randn(1,2**8)*10
+    b = np.random.randn(1,2**8)*10
+    a = a.tolist()
+    b = b.tolist()
+    a = a[0][:]
+    b = b[0][:]
 
 f = [a[i]+1J*b[i] for i in range(0,len(a))]
 
 print('FFTshift:')
-print(asc.dft.fftshift(f))
+if testRuntime == False:
+    print(asc.dft.fftshift(f))
 print('---------')
 
 
 print('System FFT')
-print(np.fft.fft(f)*1/len(f))
+if testRuntime == False:
+    print(np.fft.fft(f)*1/len(f))
 print('---------')
-print(np.fft.ifft(np.fft.fft(f)))
+if testRuntime == False:
+    print(np.fft.ifft(np.fft.fft(f)))
 print('---------')
 
 
 print('DFT:')
 print('---------')
-print(asc.dft.dft(f))
+if testRuntime == False:
+    print(asc.dft.dft(f))
 print('---------')
-print(asc.dft.idft(asc.dft.dft(f)))
+if testRuntime == False:
+    print(asc.dft.idft(asc.dft.dft(f)))
 print('---------\n')
 
 
 print('FFT recursive')
 print('---------')
-print(asc.dft.fft(f,'fwd','rec','c'))
-print(asc.dft.fft(f,'fwd','rec','py'))
+print('C Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','rec','c')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
+print('Python Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','rec','py')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
 print('---------')
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','rec','c'),'fwd','rec','c'))
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','rec','py'),'fwd','rec','py'))
+if testRuntime == False:
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','rec','c'),'fwd','rec','c'))
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','rec','py'),'fwd','rec','py'))
 print('---------\n')
 
+if testRuntime == False:
+    print('\n\n',f,'\n\n')
 
 print('FFT v1')
 print('---------')
-print(asc.dft.fft(f,'fwd','v1','c'))
-print(asc.dft.fft(f,'fwd','v1','py'))
+print('C Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','v1','c')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
+print('Python Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','v1','py')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
 print('---------')
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','v1','c'),'fwd','v1','c'))
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','v1','py'),'fwd','v1','py'))
+if testRuntime == False:
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','v1','c'),'fwd','v1','c'))
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','v1','py'),'fwd','v1','py'))
 print('---------\n')
 
-print('\n\n',f,'\n\n')
+if testRuntime == False:
+    print('\n\n',f,'\n\n')
 
 print('FFT vectorised')
 print('---------')
-print(asc.dft.fft(f,'fwd','vec','c'))
-print(asc.dft.fft(f,'fwd','vec','py'))
+print('C Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','vec','c')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
+print('Python Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','vec','py')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
 print('---------\n')
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','vec','c'),'fwd','vec','c'))
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','vec','py'),'fwd','vec','py'))
+if testRuntime == False:
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','vec','c'),'fwd','vec','c'))
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','vec','py'),'fwd','vec','py'))
 print('---------\n')
 
+if testRuntime == False:
+    print('\n\n',f,'\n\n')
 
 print('FFT v2')
 print('---------')
-print(asc.dft.fft(f,'fwd','v2','c'))
-print(asc.dft.fft(f,'fwd','v2','py'))
+print('C Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','v2','c')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
+print('Python Implementation:')
+tstart = time.time()
+F = asc.dft.fft(f,'fwd','v2','py')
+print('Elapsed Time: ',time.time()-tstart)
+if testRuntime == False:
+    print(F)
 print('---------\n')
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','v2','c'),'fwd','v2','c'))
-print(asc.dft.ifft(asc.dft.fft(f,'fwd','v2','py'),'fwd','v2','py'))
+if testRuntime == False:
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','v2','c'),'fwd','v2','c'))
+    print(asc.dft.ifft(asc.dft.fft(f,'fwd','v2','py'),'fwd','v2','py'))
 
 c = [[1,2,3,4,5,6],[1,2,3,4,5,6]]
 d = [1,2,3,4,5,6]
